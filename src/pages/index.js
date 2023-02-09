a196ba2d76682316eeaec578c92b99b8dabe602f
kishore1612 <34828486+kishore1612@users.noreply.github.com>
2023-02-09 15:18:28 +0530

    added src

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const IndexPage = () => {
+  return (
+    <Layout pageTitle="Balaji Industries">
+    <p>I'm making this by following the Gatsby Tutorial.</p>
+  </Layout>
+  )
+}
+
+export const Head = () => <title>Balaji Industries</title>
+
+export default IndexPage
